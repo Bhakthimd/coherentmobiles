1b0a4c353029b2bd8e9da275e6c6acf9ceafaacc
Bhakthimd <146520570+Bhakthimd@users.noreply.github.com>
2026-09-23 10:30:20 +0530

    Delete coherentmobiles-database/schema directory

--- coherentmobiles-database/schema/01_users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE,
-    phone VARCHAR(15) UNIQUE,
-    password_hash VARCHAR(255),
-    address TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
--- coherentmobiles-database/schema/03_brands.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE public.brands
-(
-    brand_id integer GENERATED ALWAYS AS IDENTITY,
-    brand_name character varying(100) NOT NULL,
-    CONSTRAINT brands_pkey PRIMARY KEY (brand_id)
-);
